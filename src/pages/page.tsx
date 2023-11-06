6490474476ecd6275e14c6259f9bdf39f4799a82
EnriqueB720 <enrique21102001@hotmail.com>
2023-11-06 10:35:12 -0600

    Adding children to componentns and new components

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -1,6 +1,5 @@
 import { useUserQuery } from "@/shared/generated/graphql-schema";
-import { IconButton } from "@chakra-ui/react";
-import { Button,Image, Avatar, Box, Input } from "@shared-components";
+import { Button,Image, Avatar, Box, Input, Stack, IconButton, Text } from "@shared-components";
 
 export default function Home() {
 
@@ -22,8 +21,27 @@ export default function Home() {
       <Button loading={false} variant={"outline"} text="tesdsst" color="blue"/>
       <Image src="https://bit.ly/dan-abramov" w={"400px"} h={"300px"} boxSize="100px" borderRadius='full' alt='Dan Abramov'></Image>
       <Avatar name="Enrique barroso" />
-      <Box bg="lightblue" text='text box'/>
+      <Box bg="lightgreen" style={{
+        textAlign: "center"
+      }}>
+          <Input placeholder="Example text" />
+          <Image src="https://bit.ly/dan-abramov" w={"400px"} h={"300px"} boxSize="100px" borderRadius='full' alt='Dan Abramov'></Image>
+      </Box>
       <Input placeholder="Example text" />
+      <Stack direction="column" spacing={4} align="stretch" divider={false}>
+      <Box bg="lightblue" text='text box'/>
+      <Box bg="lightblue" text='text box'/>
+      <Box bg="lightblue" text='text box'/>
+        </Stack>
+       <IconButton icon="send"></IconButton>
+      <Text as={'b'} noOfLines={2} color="green" fontSize="lg">
+       Lorem, ipsum dolor sit amet consectetur 
+       adipisicing elit. Minima animi, id illo odit nobis, fuga optio molestiae modi molestias earum harum maxime iste mollitia dolorem est magni consectetur labore suscipit.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti expedita cum totam, quisquam aperiam labore voluptatum? Consequuntur voluptas vero, quaerat maxime, 
+      fuga suscipit ipsam magni hic necessitatibus dolorum eius provident.
+        </Text> 
+
+        
       </div>
     </main>
   )
